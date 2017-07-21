15a962d0cfb599e7a12527eefee8e072e24791e6
jedjasie12 <x-wing12>
2017-07-21 13:08:11 -0400

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jasie on 7/14/17.
+ */
+
+public class HelloWorld {
+
+        public static void main(String[] args)
+        {
+            System.out.println(Math.addExact(10000, 34563456));
+            System.out.println("Hello, World!");
+            System.out.println("I'm in man");
+        }
+    }
+
+
